53f1b88a14bc47ad37c553da97a643e2844e87a8
uLarusu <ldaneliee33@gmail.com>
2026-03-21 22:00:41 +0800

    Added core functionalities: evaluateExpression, print truth table
    Refactor readThroughParenthesis function to use for printing truth table
    Add a dynamic printing for truth table that looks easy to read in cli
    Add evaluateExperssion to calculate expressions

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from propositional_logic import PropositionalLogic
 
 def main():
     logic = PropositionalLogic("(p v q) v (q ^ r)")
-    print(logic.variableCount)
-    idontknow = logic.mapVariablesToTruthValues()
-    print(f"ito na yung change: {idontknow}")
+    print(logic.printTruthTable())
 
 if __name__ == "__main__":
     main()
--- propositional_logic.py
+++ propositional_logic.py
@@ -11,6 +11,7 @@ class PropositionalLogic:
     """
     STEP 1 : IDENTIFY ALL THE UNIQUE ATOMIC VARIABLE
     """
+
     def _setAtomicVariables(self) -> str:
         atomicVariables = set() # automatically ignores duplicates
         for char in self.statement:
@@ -25,12 +26,14 @@ class PropositionalLogic:
     """
     STEP 2 : DETERMINE THE NUMBER OF ROWS
     """
+
     def _numberOfRows(self): 
         return 2 ** self.variableCount
 
     """
     STEP 3 : FILL ALL THE COLUMN WITH VALUES (0, 1)
     """
+
     def generateBinaryRows(self):
         binaryStrings = []
         binaryRows = []
@@ -54,20 +57,99 @@ class PropositionalLogic:
     """
     STEP 4 : CHANGE ALL VALUES INTO SOMETHING PYTHON CAN READ
     """
-
-    def mapVariablesToTruthValues(self) -> list:
+    # Step 4.0 : a function to convert variables to truth values
+    def _mapVariablesToTruthValues(self) -> list:
         atomicVariables = self.atomicVariables
         columnValues = self.binaryRows
         changedVariables = []
         
         # Assignign all the atomicVariables with its truth values equivalence
         # p = 0, q = 0, r = 1
-        for row in range(len(columnValues)):
+        for row in range(self.rowsCount):
             temp = {}
             for i in range(len(atomicVariables)):
-                temp[atomicVariables[i]] = int(columnValues[row][i])
+                temp[atomicVariables[i]] = (columnValues[row][i])
             changedVariables.append(temp)
 
-        return changedVariables
+        return changedVariables # returns a list of dictionary
+    
+    def _convertExpression(self, rowDict, statement) -> str:
+        # Step 4.1 : Replace each variable with its value 
+        # convert atomic variables(p, q) into binary
+        translationTable = str.maketrans(rowDict)
+        replacedText = statement.translate(translationTable)
+         
+        # Step 4.2 : replace logical operators  
+        # convert logical operators into python equivalence
+        logicalOperators = {'^': 'and', 'v': 'or', '~': 'not'}
+        for operator, equivalence in logicalOperators.items():
+            replacedText = replacedText.replace(operator, equivalence)
+        
+        return replacedText # (p v q) convert into (0 or 1) 
+
+    def evaluateExpression(self, statement = None) -> list:
+        statement = statement or self.statement
+
+        # Step 4.3 : Evaluate the resulting strings
+        # since statements are converted into statement that python can read 
+        # we can now evaluate it using eval()
+        evaluated = [] 
+        mappedVariables = self._mapVariablesToTruthValues()
+        for var in mappedVariables:
+            evaluated.append(eval(self._convertExpression(var, statement)))
+        
+        # Step 4.4 : result is 0 or 1
+        return evaluated # return list of evaluation
+    
+    """
+    STEP 5 : PRINT THE OUTPUTS
+    """
+    def readThroughParenthesis(self) -> list:
+        question = self.statement
+        firstIndex = lastIndex = 0
+        textArray = []
+        text = ""
+
+        for i in range(len(question)):
+            match question[i]:
+                case '(':
+                    firstIndex = i + 1
+                    continue
+                case ')':
+                    lastIndex = i
+
+            if(firstIndex > 0 and lastIndex > 0):
+                text = question[firstIndex:lastIndex]
+                textArray.append(text)
+                firstIndex = 0
+                lastIndex = 0
+                text = ""
+
+        return textArray
+
+    def printTruthTable(self):
+        # Header
+        sentenceLength : int = 0
+        for char in self.atomicVariables.strip():
+            print(f"{char:^3}", end="")
+        for sentence in self.readThroughParenthesis():
+            print(f"({sentence})", end=" ")
+            sentenceLength = max(len(sentence), sentenceLength)
+        print()
+        
+        # Body
+        for row in range(self.rowsCount):
+            for byte in self.binaryRows[row]:
+                print(f"{byte:^3}", end="")
+
+            destructure = self.readThroughParenthesis()
+            for chunk in destructure:
+                chunkEvaluate = [c for c in self.evaluateExpression(chunk)]
+                print(f"{chunkEvaluate[row]:^{sentenceLength+3}}", end="")
+             
+            print()
+        
+        print()
+    
 
 
